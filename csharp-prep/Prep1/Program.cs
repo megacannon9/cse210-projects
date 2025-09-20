d36c52ddb2fcc149a2a071243137743ea8254f36
megacannon9 <littlemic_6@hotmail.com>
2025-09-20 16:04:58 -0600

    Prep1&2 completion

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string First = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string Last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {Last}. {First} {Last}.");
     }
+
 }
