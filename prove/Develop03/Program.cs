fb883ab7a3c6ca2d2ec2f755f1beeb431498b396
megacannon9 <littlemic_6@hotmail.com>
2025-10-22 14:53:22 -0600

    checking accidental branch creation

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,12 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Word23 word1 = new Word23("Jesus");
-        Word23 word2 = new Word23("wept.");
-        word1.Display();
-        word2.Display();
-        word1.Hide();
-        word1.Display();
-        word2.Display();
+        
     }
 }
